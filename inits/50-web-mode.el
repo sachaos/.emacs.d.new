384b0a5fbdf765c1f0363296e73e814e5c3d50fc
sachaos <sakataku7@gmail.com>
2016-05-16 21:53:12 +0900

    Remove merge fail

--- inits/50-web-mode.el
+++ inits/50-web-mode.el
@@ -6,10 +6,6 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
-<<<<<<< 8cf952f73ad68d3c5cc427fff6b976a710a1a324
-(setq css-indent-offset 2)
-=======
->>>>>>> Modify indent settings
 
 ;; adapt filename
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
