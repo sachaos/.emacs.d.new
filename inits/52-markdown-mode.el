ebcd7bb24c71e4f805104bf584de0245c5ae8eb9
sachaos <sakataku7@gmail.com>
2016-06-02 18:44:44 +0900

    Enable orgtbl-mode on markdown

--- inits/52-markdown-mode.el
+++ inits/52-markdown-mode.el
@@ -4,6 +4,7 @@
 
 (require 'markdown-mode)
 (require 'mmm-mode)
+(require 'org-table)
 (setq mmm-global-mode 'maybe)
 
 (defun my-mmm-markdown-auto-class (lang &optional submode)
@@ -30,3 +31,12 @@ If SUBMODE is not provided, use `LANG-mode' by default."
 (set-face-foreground 'markdown-header-face-2 "brightcyan")
 (set-face-foreground 'markdown-header-face-3 "brightgreen")
 (set-face-foreground 'markdown-header-face-4 "brightyellow")
+
+(defun cleanup-org-tables ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "-+-" nil t) (replace-match "-|-"))))
+(add-hook 'markdown-mode-hook 'orgtbl-mode)
+(add-hook 'markdown-mode-hook
+          #'(lambda()
+              (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)))
