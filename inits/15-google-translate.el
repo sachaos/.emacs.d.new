bad01bcb6af93d5f53fe7b9903eeb624d587ac0e
sachaos <sakataku7@gmail.com>
2016-06-03 12:59:36 +0900

    Add google-translate.el

---
+++ inits/15-google-translate.el
@@ -0,0 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;
+;; google-translate ;;
+;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'google-translate)
+
+(setq google-translate-default-source-language "en")
+(setq google-translate-default-target-language "ja")
