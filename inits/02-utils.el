71e3de9c06f059ac80da393d5a3e6007f9ea6d26
sachaos <sakataku7@gmail.com>
2016-06-13 09:56:26 +0900

    Add current-date-time function

--- inits/02-utils.el
+++ inits/02-utils.el
@@ -12,6 +12,9 @@ See help of `format-time-string' for possible replacements")
   "Format of date to insert with `insert-current-time' func.
 Note the weekly scope of the command's precision.")
 
+(defvar current-date-time-format "%Y/%m/%d %H:%M:%S"
+  "Format of datetime to insert with `insert-current-date-time' func.")
+
 (defun insert-current-date ()
   "insert the current date and time into current buffer.
 Uses `current-date-format' for the formatting the date/time."
@@ -24,5 +27,13 @@ Uses `current-date-format' for the formatting the date/time."
   "insert the current time (1-week scope) into the current buffer."
   (interactive)
   (insert (format-time-string current-time-format (current-time)))
+  (insert "\n")
+  )
+
+(defun insert-current-date-time ()
+  "insert the current date and time into current buffer.
+Uses `current-date-time-format' for the formatting the date/time"
+  (interactive)
+  (insert (format-time-string current-date-time-format (current-time)))
   (insert "\n")
   )
