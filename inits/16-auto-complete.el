dfdc92defb8e14239e0dd87f6a56db5793842f5a
sachaos <sakataku7@gmail.com>
2016-06-20 01:17:42 +0900

    Add ac settings to enable ac anywhere

--- inits/16-auto-complete.el
+++ inits/16-auto-complete.el
@@ -6,3 +6,8 @@
 (setq ac-use-menu-map t)
 (setq ac-use-fuzzy t)
 (global-auto-complete-mode t)
+
+(defun auto-complete-mode-maybe ()
+  "No maybe for you. Only AC!"
+  (unless (minibufferp (current-buffer))
+    (auto-complete-mode 1)))
