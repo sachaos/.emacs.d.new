6e51e16949bf0b33211038a97d210a8db820bc81
sachaos <sakataku7@gmail.com>
2016-07-18 14:04:58 +0900

    Enable web-mode on .eex files

--- inits/98-auto-mode-alias.el
+++ inits/98-auto-mode-alias.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tag$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex$" . web-mode))
 
 ;; js2-jsx-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
