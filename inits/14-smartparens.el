c045236521c8f9c58866ee73b1ab05121717b0c4
sachaos <sakataku7@gmail.com>
2016-08-07 16:32:39 +0900

    Add adhoc parens

--- inits/14-smartparens.el
+++ inits/14-smartparens.el
@@ -14,3 +14,14 @@
                  :when '(("SPC" "RET"))
                  :post-handlers '(sp-ruby-def-post-handler)
                           :actions '(insert navigate)))
+
+(sp-with-modes '(web-mode)
+  (sp-local-pair "<%" "%>"
+                 :when '(("SPC" "RET"))
+                 :actions '(insert navigate))
+  (sp-local-pair "<%=" "%>"
+                 :when '(("SPC" "RET"))
+                 :actions '(insert navigate))
+  (sp-local-pair "<" nil
+                 :when '(("SPC" "RET"))))
+
